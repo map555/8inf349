e26faf5d1fd7f1bad84307c3b58c656db9adbf3e
Marc-Antoine Plourde <marc-antoine.plourde1@uqac.ca>
2021-02-19 23:30:05 -0500

    Create __init__.py in the Api8inf349 directory

---
+++ Api8inf349/__init__.py
@@ -0,0 +1,11 @@
+
+
+from flask import Flask, request, redirect, url_for, abort
+
+from Api8inf349.models import init_app, Product, Transaction, CreditCard, ShippingInformation
+
+
+def create_app(initial_config=None):
+    app = Flask("8inf439")
+    init_app(app)
+    return app
