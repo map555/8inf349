2e21240e6b9707043c437a372f8e10072073e16c
Marc-Antoine Plourde <marc-antoine.plourde1@uqac.ca>
2021-02-19 23:32:55 -0500

    Add the conftest and the functionnal test file

---
+++ test/conftest.py
@@ -0,0 +1,20 @@
+import os
+import tempfile
+
+os.environ['DATABASE'] = ":memory:"
+import pytest
+from peewee import SqliteDatabase
+from Api8inf349 import create_app
+from Api8inf349.models import get_db_path, Product, ShippingInformation, CreditCard, Transaction, Order
+
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    db = SqliteDatabase(get_db_path())
+    db.create_tables([Product, ShippingInformation, CreditCard, Transaction, Order])
+
+    yield app
+
+    db.drop_tables([Product, ShippingInformation, CreditCard, Transaction, Order])
---
+++ test/test_functionnal.py
@@ -0,0 +1,10 @@
+import pytest
+from Api8inf349.models import Product
+
+
+class TestProduct(object):
+    def test_rating(self, app):
+        with app.app_context():
+            p = Product(id=1, name="Brown eggs", type="dairy", description="Raw organic brown eggs in a basket",
+                        image="0.jpg", height=600, weight=400, price=28.1, rating=8, in_stock=False)
+            p.save()
