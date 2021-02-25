9aa7b98681a7de1daf2cc4fbbb1d86b823db3598
marc <marc-antoine.plourde1@uqac.ca>
2021-02-25 11:57:04 -0500

    Update __init__.py

--- Api8inf349/__init__.py
+++ Api8inf349/__init__.py
@@ -1,7 +1,4 @@
-
-
 from flask import Flask, request, redirect, url_for, abort
-
 from Api8inf349.models import init_app, Product, Transaction, CreditCard, ShippingInformation
 
 
@@ -9,3 +6,5 @@ def create_app(initial_config=None):
     app = Flask("Api8inf439")
     init_app(app)
     return app
+
+
