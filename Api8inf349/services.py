912a2bc74945de69e2798615f8b6311599c0af96
marc <marc-antoine.plourde1@uqac.ca>
2021-02-25 12:09:58 -0500

    implement getOrder and getOrderDict class method for OrderServices

--- Api8inf349/services.py
+++ Api8inf349/services.py
@@ -90,3 +90,38 @@ class OrderServices(object):
             response['object'] = errordict
             return response
 
+
+    @classmethod
+    def getOrder(cls, id):
+
+        order = Order.get_or_none(Order.id == id)
+
+        return order
+
+    @classmethod
+    def getOrderDict(cls, id):
+        order = Order.get_or_none(Order.id == id)
+
+        jsonOrder = {'id': order.id, 'shipping_information': {}, 'credit_card': {}, 'email': order.email,
+                     'total_price': order.total_price, 'transaction': {}, 'paid': order.paid, 'product':
+                         {'product_id': order.product.id, 'product_quantity': order.product_quantity}, 'shipping_price':
+                         order.shipping_price}
+
+        if order.shipping_information is not None:
+            jsonOrder['shipping_information'] = {'id': order.shipping_information.id, 'country':
+                order.shipping_information.country, 'address': order.shipping_information.adress, 'postal_code':
+                                                     order.shipping_information.postal_code,
+                                                 'city': order.shipping_information.city, 'province':
+                                                     order.shipping_information.province}
+
+        if order.credit_card is not None:
+            jsonOrder['credit_card'] = {'id': order.credit_card.id, 'name': order.credit_card.name, 'number':
+                order.credit_card.number, 'expiration_month': order.credit_card.expiration_month, 'expiration_year':
+                                            order.credit_card.expiration_year, 'cvv': order.credit_card.cvv}
+
+        if order.transaction is not None:
+            jsonOrder['transaction'] = {'id': order.transaction.id, 'success': order.transaction.success,
+                                        'amount_charged':
+                                            order.transaction.amount_charged}
+
+        return jsonOrder
