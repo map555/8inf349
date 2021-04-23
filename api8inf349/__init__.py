6bf157e9e1df1ee4a5e3ea7af2a5516f222b2d69
marc <marc-antoine.plourde1@uqac.ca>
2021-04-23 00:05:01 -0400

    Update __init__.py

--- api8inf349/__init__.py
+++ api8inf349/__init__.py
@@ -13,7 +13,10 @@ import click
 def create_app():
     app = Flask("api8inf349")
     init_app(app)
-    InitializeProduct()
+    try:
+        InitializeProduct()
+    except():
+        pass
 
     queue = Queue(connection=getRedis())
 
