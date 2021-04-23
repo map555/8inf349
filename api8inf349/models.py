fc5b36fa8cfddab94c71945134bf249b728a9407
marc <marc-antoine.plourde1@uqac.ca>
2021-04-23 02:37:56 -0400

    Create a InitProductTable class from the functions in product_table_init.py

--- api8inf349/models.py
+++ api8inf349/models.py
@@ -9,14 +9,10 @@ from api8inf349.schemas_validation import ValidateProductListSchema
 from api8inf349.url import productURL
 from api8inf349.db import getDB
 
+
 # FLASK_DEBUG=True FLASK_APP=Api8inf349 REDIS_URL=redis://localhost DB_HOST=localhost DB_USER=user DB_PASSWORD=pass DB_PORT=5432 DB_NAME=api8inf349 flask init-db
 # set FLASK_DEBUG=True& set FLASK_APP=api8inf349& set REDIS_URL=redis://localhost& set DB_HOST=localhost& set DB_USER=user& set DB_PASSWORD=pass& set DB_PORT=5432& set DB_NAME=api8inf349
 # docker run -p 5000:5000 -e REDIS_URL=redis://host.docker.internal -e DB_HOST=host.docker.internal -e DB_USER=user -e DB_PASSWORD=pass -e DB_PORT=5432 -e DB_NAME=api8inf349 api8inf349
-
-
-
-
-
 
 
 class BaseModel(Model):
@@ -118,68 +114,65 @@ class PaymentError(BaseModel):
     time = TimestampField()
 
 
+# Because we can init table with the old InitializeProduct function,
+# we create a InitProductTable class in model.py to be able to call it in the init-db method
+class InitProductTable:
+    def __init__(self):
+        self.__url = productURL
+        self.__productsDict = self.__getProducts()
+        self.__UpdateProduct()  # check if update
 
-def getRequest(url):
-    r = Request(productURL)
-    response = urlopen(r)
-    code = response.getcode()
-    if code != 200:
-        print("ERROR: trying to get products from \"http://jgnault.ddns.net/shops/products/\" return http code", code)
-        exit(0)
-    return response
+    def __getRequest(self):
+        r = Request(self.__url)
+        response = urlopen(r)
+        code = response.getcode()
+        if code != 200:
+            print("ERROR: trying to get products from \"http://jgnault.ddns.net/shops/products/\" return http code",
+                  code)
+            exit(0)
+        return response
 
+    def __ConvertResponseToJson(self, response):
+        jsn = None
+        try:
+            jsn = json.loads(response.read())
+        except Exception as exept:
+            print(exept)
+        # fix the problem with the null char for product id 45
+        jsn['products'][44][
+            'description'] = "Healthy breakfast set. rice cereal or porridge with fresh strawberry, apricots," \
+                             "almond and honey over white rustic wood backdrop, top view."
 
-def ConvertResponseToJson(response):
-    jsn = None
-    try:
-        jsn = json.loads(response.read())
-    except Exception as exept:
-        print(exept)
-    # fix the problem with the null char for product id 45
-    jsn['products'][44][
-        'description'] = "Healthy breakfast set. rice cereal or porridge with fresh strawberry, apricots," \
-                         "almond and honey over white rustic wood backdrop, top view."
+        return jsn
 
-    return jsn
+    def __getProducts(self):
+        response = self.__getRequest()
+        jsonDict = self.__ConvertResponseToJson(response=response)
+        return jsonDict
 
+    def __CheckExistance(self, jsonProduct):
+        id = int(jsonProduct['id'])
 
-def getProducts():
-    response = getRequest(url=productURL)
-    jsonDict = ConvertResponseToJson(response=response)
-    return jsonDict
-
-
-def CheckExistance(jsonProduct):
-    id = int(jsonProduct['id'])
-
-    try:
-        query = Product.get_or_none(Product.id == id)
-        if query is not None:
-            return True
-        else:
+        try:
+            query = Product.get_or_none(Product.id == id)
+            if query is not None:
+                return True
+            else:
+                return False
+        except:
             return False
-    except:
-        return False
 
-
-# we dont test this one because CheckExistance and bd insert are already tested.
-def UpdateProduct(ProductsDict):
-    if ValidateProductListSchema(ProductsDict) is False:
-        print("ERROR: INVALID PRODUCT!\nTHE PROGRAM WILL NOW EXIT.")
-        exit(0)
-    for p in ProductsDict['products']:
-        checkExist = CheckExistance(p)
-        if checkExist is not True:
-            Product.create(name=p['name'], type=p['type'], description=p['description'], image=p['image'],
-                           height=p['height'], weight=p['weight'], price=p['price'], rating=p['rating'],
-                           in_stock=p['in_stock'])
-
-
-def InitializeProduct():
-    products = getProducts()
-    UpdateProduct(ProductsDict=products)  # check if update
-
-
+    # we dont test this one because CheckExistance and bd insert are already tested.
+    def __UpdateProduct(self):
+        if ValidateProductListSchema(self.__productsDict) is False:
+            print("ERROR: INVALID PRODUCT!\nTHE PROGRAM WILL NOW EXIT.")
+            exit(0)
+        for p in ProductsDict['products']:
+            checkExist = CheckExistance(p)
+            if checkExist is not True:
+                Product.create(name=p['name'], type=p['type'], description=p['description'], image=p['image'],
+                               height=p['height'], weight=p['weight'], price=p['price'], rating=p['rating'],
+                               in_stock=p['in_stock'])
 
 
 @click.command("init-db")
@@ -187,10 +180,9 @@ def InitializeProduct():
 def init_db_command():
     db = getDB()
     db.create_tables([Product, ShippingInformation, CreditCard, Transaction, Order, ProductOrdered, PaymentError])
-    InitializeProduct()
+    InitProductTable()
     click.echo("Initialized the database.")
 
 
 def init_app(app):
     app.cli.add_command(init_db_command)
-
